c675242c9cede8acc034fe72b62da44f4731a5f4
Karinne Gomes <39595884+kah-g@users.noreply.github.com>
2023-05-30 22:31:25 +0100

    Add files via upload

---
+++ Programacao_Extrema/PrimeiroPrograma.java
@@ -0,0 +1,22 @@
+//classe
+class PrimeiroPrograma
+{
+    //modulo principal
+    public static void main (string dk[])
+    {
+        //decalracao de variaveis
+        int inteiro = 101;
+        char character = 'p';
+        double real = 2.11;
+        String frase = "josislane";
+        boolean VF = true;
+
+        if (VF == true)
+        {
+            System.out.println ("Eu sou a " + frase + " e tenho " + inteiro + " anos e tenho " + real + " de altura");
+
+        }
+        System.exit(0);
+
+    }
+}
